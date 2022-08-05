eeb6366b1fc98c167a9787b17261b777536eb697
vishalsingh <tan.vishalsingh@gmail.com>
2022-08-05 12:54:31 +0530

    updated

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,20 +1,23 @@
 import logging
 from datetime import datetime
 import os
+import pandas as pd
+from housing.constant import get_current_time_stamp 
+LOG_DIR="logs"
 
-LOG_DIR="housing_logs"
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
 
-CURRENT_TIME_STAMP= f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
-
-LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
+LOG_FILE_NAME=get_log_file_name()
 
 os.makedirs(LOG_DIR,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
 
+
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode="w",
-format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s' ,
+format='[%(asctime)s]^;%(levelname)s^;%(lineno)d^;%(filename)s^;%(funcName)s()^;%(message)s',
 level=logging.INFO
 )
--- housing/pipeline/pipeline.py
+++ housing/pipeline/pipeline.py
@@ -1,11 +1,11 @@
-
-
-
-
-from tkinter import E
 from housing.config.configuration import Configuartion
+from housing.logger import logging, get_log_file_name
 from housing.exception import HousingException
 
+from housing.entity.artifact_entity import DataIngestionArtifact
+from housing.entity.config_entity import DataIngestionConfig
+from housing.component.data_ingestion import DataIngestion
+import os,sys
 
 class Pipeline():
     
