a27e84724c0d75f352b55c25af100d9b0432f403
vishalsingh <tan.vishalsingh@gmail.com>
2022-08-31 13:32:39 +0530

    updated

--- demo.py
+++ demo.py
@@ -2,17 +2,19 @@ from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
 from housing.config.configuration import Configuartion
-
+from housing.component.data_transformation import DataTransformation
 import os
+
+
 def main():
     try:
-        #config_path = os.path.join("config","config.yaml") 
-        #pipeline = Pipeline(Configuartion(config_file_path=config_path))
-        #pipeline.run_pipeline()
+        config_path = os.path.join("config","config.yaml") 
+        pipeline = Pipeline(Configuartion(config_file_path=config_path))
+        pipeline.run_pipeline()
         
         #logging.info("main function execution completed.")
-        data_validation_config = Configuartion().get_data_transformation_config()
-        print(data_validation_config)
+        #data_validation_config = Configuartion().get_data_transformation_config()
+        #print(data_validation_config)
         # schema_file_path=r"D:\Project\machine_learning_project\config\schema.yaml"
         # file_path=r"D:\Project\machine_learning_project\housing\artifact\data_ingestion\2022-06-27-19-13-17\ingested_data\train\housing.csv"
 
